6804d96575ef8c7300524fbcbdcc3400caf9a5c1
Roberto Barros <roberto.f.barros@gmail.com>
2019-08-22 23:28:20 -0300

    product show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,15 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show]
   def index
     @products = Product.in_auction.all
   end
 
   def show
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
